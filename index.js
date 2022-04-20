b15c1c387f7c560ec29da9b876b7a0b25dcb75e4
danmousley367 <dan.mousley@softwire.com>
2022-04-20 17:03:38 +0100

    Completed task 2.2

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ for (let i = 1; i <= 100; i++) {
     if (i % 7 == 0) {
         output += "Bang"
     }
+    if (i % 11 == 0) {
+        output = "Bong"
+    }
     console.log(output.length ? output : i)
 }
