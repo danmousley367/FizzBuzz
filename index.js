d363782e46ca4d82c048616d54b7269633db929a
danmousley367 <dan.mousley@softwire.com>
2022-04-20 17:02:07 +0100

    Completed task 2.1

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FizzBuzz")
-    } else if (i % 3 == 0) {
-        console.log("Fizz")
-    } else if (i % 5 == 0) {
-        console.log("Buzz")
-    } else {
-        console.log(i)
+    let output = ""
+    if (i % 3 == 0) {
+        output += "Fizz"
     }
+    if (i % 5 == 0) {
+        output += "Buzz"
+    }
+    if (i % 7 == 0) {
+        output += "Bang"
+    }
+    console.log(output.length ? output : i)
 }
