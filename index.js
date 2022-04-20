1806ec5666bc01709c0bd48828604f485a9ccdbb
danmousley367 <dan.mousley@softwire.com>
2022-04-20 17:28:22 +0100

    Completed task 2.3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 200; i++) {
     let output = ""
     if (i % 3 == 0) {
         output += "Fizz"
@@ -11,6 +11,10 @@ for (let i = 1; i <= 100; i++) {
     }
     if (i % 11 == 0) {
         output = "Bong"
+    }
+    if (i % 13 == 0) {
+        let index = output.indexOf("B")
+        output = index == -1 ? output + "Fezz" : output.slice(0, index) + "Fezz" + output.slice(index, output.length)
     }
     console.log(output.length ? output : i)
 }
